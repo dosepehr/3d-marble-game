4107d634756214cb4f2e536bf63e7deaf9b4b5f6
dosepehr <dosepehr@gmail.com>
2023-08-12 07:02:32 +0330

    add horizental block

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -2,14 +2,16 @@ import BlockVertical from './BlockVertical';
 import BlockSpinner from './BlockSpinner';
 import BlockStart from './BlockStart';
 import { Physics, Debug } from '@react-three/rapier';
+import BlockHorizontal from './BlockHorizontal';
 const Level = () => {
     return (
         <>
             <Physics>
                 <Debug />
-                <BlockStart position={[0, 0, 8]} scale={[4, 0.2, 4]} />
-                <BlockSpinner position={[0, 0, 4]} scale={[4, 0.2, 4]} />
-                <BlockVertical position={[0, 0, 0]} scale={[4, 0.2, 4]} />
+                <BlockStart position={[0, 0, 12]} scale={[4, 0.2, 4]} />
+                <BlockSpinner position={[0, 0, 8]} scale={[4, 0.2, 4]} />
+                <BlockVertical position={[0, 0, 4]} scale={[4, 0.2, 4]} />
+                <BlockHorizontal position={[0, 0, 0]} scale={[4, 0.2, 4]} />
             </Physics>
         </>
     );
