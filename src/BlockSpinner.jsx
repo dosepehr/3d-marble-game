cb8ad1512022a8abf81cf50c025c9c02085a6cdf
dosepehr <dosepehr@gmail.com>
2023-08-12 06:17:06 +0330

    add spinner to the Block

--- src/BlockSpinner.jsx
+++ src/BlockSpinner.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import { boxGeometry, floor2Material } from './helpers';
-
+import { boxGeometry, floor2Material, obstacleMaterial } from './helpers';
 const BlockSpinner = ({ position = [0, 0, 0], scale = [1, 1, 1] }) => {
     return (
         <>
@@ -10,6 +8,14 @@ const BlockSpinner = ({ position = [0, 0, 0], scale = [1, 1, 1] }) => {
                     geometry={boxGeometry}
                     material={floor2Material}
                     position-y={-0.1}
+                    receiveShadow
+                ></mesh>
+
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[3.5, 0.3, 0.3]}
+                    castShadow
                     receiveShadow
                 />
             </group>
