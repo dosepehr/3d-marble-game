562e83a58bce86af653f549d4fe9e50fcd57e9b3
dosepehr <dosepehr@gmail.com>
2023-08-12 06:02:12 +0330

    add Stats for performances

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { OrbitControls } from '@react-three/drei';
 import Level from './Level';
 import Lights from './Lights';
+import Stats from './Stats';
 const App = () => {
     return (
         <>
             <OrbitControls makeDefault />
             <Level />
+            <Stats />
             <Lights />
         </>
     );
