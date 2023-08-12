34a2898f3c2cbba80777f296e80f6410e66bcf8e
dosepehr <dosepehr@gmail.com>
2023-08-12 05:46:42 +0330

    add common materials and geometries

---
+++ src/helpers.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three';
+
+export const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+export const floor1Material = new THREE.MeshStandardMaterial({
+    color: 'limegreen',
+});
+export const floor2Material = new THREE.MeshStandardMaterial({
+    color: 'greenyellow',
+});
+export const obstacleMaterial = new THREE.MeshStandardMaterial({
+    color: 'orangered',
+});
+export const wallMaterial = new THREE.MeshStandardMaterial({
+    color: 'slategrey',
+});
