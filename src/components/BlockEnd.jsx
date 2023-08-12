22fc369777383f603915d3ed46d7d73210f8e9f2
dosepehr <dosepehr@gmail.com>
2023-08-12 07:28:34 +0330

    add end block

---
+++ src/components/BlockEnd.jsx
@@ -0,0 +1,48 @@
+import { boxGeometry, floor1Material } from './helpers';
+import { useFrame } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
+import { RigidBody, CylinderCollider } from '@react-three/rapier';
+import { useRef } from 'react';
+
+
+const BlockEnd = ({ position = [0, 0, 0], scale = [1, 1, 1] }) => {
+    const hamburger = useRef();
+    useFrame((state, delta) => {
+        hamburger.current.rotation.y += delta;
+    });
+    const hamburgerModel = useGLTF('./hamburger.glb');
+    hamburgerModel.scene.children.forEach((mesh) => {
+        mesh.castShadow = true;
+    });
+    return (
+        <>
+            <group position={position}>
+                <mesh
+                    scale={scale}
+                    geometry={boxGeometry}
+                    material={floor1Material}
+                    receiveShadow
+                />
+                <RigidBody
+                    type='fixed'
+                    colliders={false}
+                    restitution={0.2}
+                    friction={0}
+                    position={[0, 0.25, 0]}
+                >
+                    <CylinderCollider
+                        args={[0.5, 1.25]}
+                        position={[0, 0.5, 0]}
+                    />
+                    <primitive
+                        ref={hamburger}
+                        object={hamburgerModel.scene}
+                        scale={0.2}
+                    />
+                </RigidBody>
+            </group>
+        </>
+    );
+};
+
+export default BlockEnd;
--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,17 +1,19 @@
 import BlockVertical from './BlockVertical';
 import BlockSpinner from './BlockSpinner';
 import BlockStart from './BlockStart';
-import { Physics, Debug } from '@react-three/rapier';
 import BlockHorizontal from './BlockHorizontal';
+import BlockEnd from './BlockEnd';
+import { Physics, Debug } from '@react-three/rapier';
 const Level = () => {
     return (
         <>
             <Physics>
                 <Debug />
-                <BlockStart position={[0, 0, 12]} scale={[4, 0.2, 4]} />
-                <BlockSpinner position={[0, 0, 8]} scale={[4, 0.2, 4]} />
-                <BlockVertical position={[0, 0, 4]} scale={[4, 0.2, 4]} />
-                <BlockHorizontal position={[0, 0, 0]} scale={[4, 0.2, 4]} />
+                <BlockStart position={[0, 0, 16]} scale={[4, 0.2, 4]} />
+                <BlockSpinner position={[0, 0, 12]} scale={[4, 0.2, 4]} />
+                <BlockVertical position={[0, 0, 8]} scale={[4, 0.2, 4]} />
+                <BlockHorizontal position={[0, 0, 4]} scale={[4, 0.2, 4]} />
+                <BlockEnd position={[0, 0, 0]} scale={[4, 0.2, 4]} />
             </Physics>
         </>
     );
