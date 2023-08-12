2e8ad181b265e725bf9e9e8f863f7513f77cf274
dosepehr <dosepehr@gmail.com>
2023-08-12 08:17:56 +0330

    add floor collider

--- src/components/Walls.jsx
+++ src/components/Walls.jsx
@@ -1,5 +1,5 @@
 import { boxGeometry, wallMaterial } from './helpers';
-import { RigidBody } from '@react-three/rapier';
+import { RigidBody, CuboidCollider } from '@react-three/rapier';
 const Walls = ({ length = 1 }) => {
     return (
         <>
@@ -27,6 +27,12 @@ const Walls = ({ length = 1 }) => {
                     geometry={boxGeometry}
                     scale={[4, 1.5, 0.3]}
                     position={[0, 0.75, -(length * 4) + 2]}
+                />
+                <CuboidCollider
+                    args={[2, 0.1, 2 * length]}
+                    position={[0, -0.1, -(length * 2) + 2]}
+                    restitution={0.2}
+                    friction={1}
                 />
             </RigidBody>
         </>
