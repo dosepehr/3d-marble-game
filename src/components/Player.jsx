51bc7049dc5e87d31e1226815cd6bbb8ffa8a6c7
dosepehr <dosepehr@gmail.com>
2023-08-14 06:10:24 +0330

    add keys diractions

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,42 @@
-import { RigidBody } from '@react-three/rapier';
+import { RigidBody, useRapier } from '@react-three/rapier';
+import { useFrame } from '@react-three/fiber';
+import { useKeyboardControls } from '@react-three/drei';
+import { useRef, useEffect } from 'react';
 const Player = () => {
+    const marble = useRef();
+    const [subscribeKeys, getKeys] = useKeyboardControls();
+
+    useFrame((state, delta) => {
+        const { forward, backward, leftward, rightward } = getKeys();
+        const impulse = { x: 0, y: 0, z: 0 };
+        const torque = { x: 0, y: 0, z: 0 };
+        const impulseStrength = 0.6 * delta;
+        const torqueStrength = 0.2 * delta;
+        if (forward) {
+            impulse.z -= impulseStrength;
+            torque.x -= torqueStrength;
+        }
+        if (rightward) {
+            impulse.x += impulseStrength;
+            torque.z -= torqueStrength;
+        }
+        if (backward) {
+            impulse.z += impulseStrength;
+            torque.x += torqueStrength;
+        }
+        if (leftward) {
+            impulse.x -= impulseStrength;
+            torque.z += torqueStrength;
+        }
+        marble.current.applyImpulse(impulse);
+        marble.current.applyTorqueImpulse(torque);
+    });
     return (
         <>
             <RigidBody
+                linearDamping={0.5}
+                angularDamping={0.5}
+                ref={marble}
                 position={[0, 1, 0]}
                 colliders='ball'
                 restitution={0.2}
--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,29 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Canvas } from '@react-three/fiber';
+import { KeyboardControls } from '@react-three/drei';
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Canvas
-            shadows
-            camera={{
-                fov: 45,
-                near: 0.1,
-                far: 200,
-                position: [2.5, 4, 6],
-            }}
+        <KeyboardControls
+            map={[
+                { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+                { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+                { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+                { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+                { name: 'jump', keys: ['Space'] },
+            ]}
         >
-            <App />
-        </Canvas>
+            <Canvas
+                shadows
+                camera={{
+                    fov: 45,
+                    near: 0.1,
+                    far: 200,
+                    position: [2.5, 4, 6],
+                }}
+            >
+                <App />
+            </Canvas>
+        </KeyboardControls>
     </StrictMode>
 );
-
