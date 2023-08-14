f508608762b7740449c73cb4dbffbb62e5402fe6
dosepehr <dosepehr@gmail.com>
2023-08-14 07:26:19 +0330

    add game interface component

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import { Canvas } from '@react-three/fiber';
 import { KeyboardControls } from '@react-three/drei';
+import Interface from './components/InterFace';
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <KeyboardControls
@@ -27,5 +28,6 @@ createRoot(document.getElementById('root')).render(
                 <App />
             </Canvas>
         </KeyboardControls>
+        <Interface />
     </StrictMode>
 );
