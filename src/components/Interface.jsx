e9768a78d45b0fec555ff49e9eab9e925629d567
dosepehr <dosepehr@gmail.com>
2023-08-15 02:40:18 +0330

    handle restart btn

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,10 +1,14 @@
 import { useKeyboardControls } from '@react-three/drei';
+import useGame from '../hooks/useGame';
 const Interface = () => {
     const forward = useKeyboardControls((state) => state.forward);
     const backward = useKeyboardControls((state) => state.backward);
     const leftward = useKeyboardControls((state) => state.leftward);
     const rightward = useKeyboardControls((state) => state.rightward);
     const jump = useKeyboardControls((state) => state.jump);
+
+    const restart = useGame((state) => state.restart);
+    const phase = useGame((state) => state.phase);
 
     return (
         <>
@@ -13,7 +17,12 @@ const Interface = () => {
                 <div className='time'>0.00</div>
                 {/* Restart */}
 
-                <div className='restart'>Restart</div>
+                {phase === 'ended' && (
+                    <div className='restart' onClick={restart}>
+                        Restart
+                    </div>
+                )}
+
                 {/* Controls */}
                 <div className='controls'>
                     <div className='raw'>
